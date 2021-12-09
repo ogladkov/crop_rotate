d691c225813947ad9dddb975e0d665ca3622ab06
ogladkov <fg404@me.com>
2021-12-09 11:00:33 +0300

    bug fixes

--- segmentation-infer.py
+++ segmentation-infer.py
@@ -19,7 +19,7 @@ class CFG:
     img_width = 512
     img_size = (img_height, img_width)
     batch_size = 4
-    device = 'cuda'
+    device = 'cpu'
     epochs = 40
     classes = ['photos', 'bg']
     encoder = 'se_resnext50_32x4d'
